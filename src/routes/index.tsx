229bbf1d2785b5d7d49afc09e1f7ef6bcca83842
AbdulHameedi <sanilinity507@gmail.com>
2025-01-10 18:42:47 +0100

    Fix: Router Error

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import LandingPage from "../pages/landing-page/LandingPage";
-import App from "../App";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
     children: [
         {
             index: true,
