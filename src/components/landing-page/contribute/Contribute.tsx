c13a8ec1aff453695afc8e343306a5fed0aac74c
AbdulHameedi <sanilinity507@gmail.com>
2025-01-15 19:37:19 +0100

    feat:contribute

--- src/components/landing-page/contribute/Contribute.tsx
+++ src/components/landing-page/contribute/Contribute.tsx
@@ -1,19 +1,37 @@
+// import { ArrowRight } from "../../../assets/svg";
+import ContributeCard from "./ContributeCard";
+
 const Contribute = () => {
   return (
     <div className="px-4">
-      <div className="flex flex-col items-center gap-3">
-        <h1 className="text-4xl font-medium text-[#D9432C]">Make Contribution</h1>
+      <div className="flex flex-col items-center gap-3 mb-10">
+        <h1 className="text-4xl font-medium text-[#D9432C]">
+          Make Contribution
+        </h1>
         <p className="text-xl text-gray-600">Join hands to build together</p>
       </div>
-      <main className="grid grid-cols-3">
-        <div className="flex flex-col items-center text-center">
-          <p>STEP 1</p>
-          <p>Create a free account</p>
-          <p className="max-w-[280px]">
-            Get up and running in no time by creating a free account. Register
-            only with your email address.
-          </p>
-        </div>
+      <main className="grid grid-cols-3 relative gap-3">
+        <ContributeCard
+          heading="Explore Projects"
+          description="Discover projects that align with your interests and skills. Browse through our repositories"
+          step={1}
+          linkText="Start Exploring"
+        />
+        <ContributeCard
+          heading="Pick an Issue"
+          description="Choose a task or issue marked for contributors. Focus on “good first issues” to get started easily"
+          step={2}
+          linkText="View Issues"
+        />
+        <ContributeCard
+          heading="Submit a Pull Request"
+          description="Make your contribution by submitting a pull request. Share your improvements or solutions for review"
+          step={3}
+          linkText="Contribute Now"
+        />
+        {/* <p className="absolute p-3 rounded-full w-fit bg-gray-500 top-[50%] left-[20rem]">
+          <ArrowRight className="text-white" />
+        </p> */}
       </main>
     </div>
   );
