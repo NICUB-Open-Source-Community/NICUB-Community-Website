07c68e926920bb87a41b0714f614b87600968b88
Adepoju Wasiu <136388170+AdepojuWasiu@users.noreply.github.com>
2025-01-26 23:15:10 +0100

    Errorcorrection (#19)
    * About and Project Pages
    
    * Error Correction

--- src/components/navigation/navbar/Navbar.tsx
+++ src/components/navigation/navbar/Navbar.tsx
@@ -2,45 +2,49 @@ import { Link } from "react-router-dom";
 import { navItems } from "../navItems";
 import SubNav from "./SubNav";
 import { ArrowRight } from "../../../assets/svg";
+import { NavLink } from "react-router-dom";
 
 interface ArrowRightProps {
   classname?: string;
-  text: string;
+  text:string;
 }
 const Navbar = () => {
   const { resources, community } = navItems[0];
   const hoverStyles =
     "hover:bg-gray-100 rounded-2xl py-1 sm:px-4  transition duration-100 cursor-pointer";
   return (
-    <section className="bg-white fixed top-0 w-full border-b z-30">
-      <nav className="flex justify-between items-center gap-0 px-4 max-w-[1024px] mx-auto py-4">
-        <Link to="/">
-          <img src="/logo.jpeg" alt="logo" className="w-[100px] h-[80px]" />
-        </Link>
-        <div className="flex items-center gap-1">
-          <p className={hoverStyles}>About Us</p>
-          <div className="relative group">
-            <p className={hoverStyles}>Community</p>
-            <SubNav items={community} />
-          </div>
-          <div className="relative group">
-            <p className={hoverStyles}>Resources</p>
-            <SubNav items={resources} />
-          </div>
-          <p className={hoverStyles}>Sponsor</p>
-          <JoinUs classname="hidden" text="Join Us" />
+    <nav className="flex justify-between items-center gap-0 px-[50px] mx-auto py-4 shadow-lg ">
+      <Link to="/"> 
+        <img src="/logo.jpeg" alt="logo" className="w-[100px] h-[70px]" />
+      </Link>
+      <div className="flex items-center gap-1">
+       <NavLink to="/about"><p className={hoverStyles}>About Us</p></NavLink>
+       <div className="relative group">
+          <NavLink to="/resources"><p className={hoverStyles}>Resources</p></NavLink>
+          <SubNav items={resources} />
         </div>
-      </nav>
-    </section>
+        <NavLink to="/projects"><p className={hoverStyles}>Projects</p></NavLink>
+        <div className="relative group">
+          <NavLink to="/community"><p className={hoverStyles}>Community</p></NavLink>
+          <SubNav items={community} />
+        </div>
+        <NavLink to="/events"><p className={hoverStyles}>Events</p></NavLink>
+        <JoinUs classname="hidden" text="Contact Us" />
+      </div>
+    </nav>
   );
 };
+
 
 export default Navbar;
 export const JoinUs = ({ classname, text }: ArrowRightProps) => {
   return (
-    <div className="w-fit bg-[#D9432C] text-white rounded-2xl py-1 px-4 cursor-pointer hover:bg-[#EE624D] transition duration-300 flex items-center gap-2">
-      <p> {text} </p>
-      <ArrowRight className={classname} />
-    </div>
+    <NavLink to="/contact-us">
+          <div className="w-fit bg-[#D9432C] text-white rounded-2xl py-1 px-4 cursor-pointer hover:bg-[#EE624D] transition duration-300 flex items-center gap-2">
+                  <p>{text}</p>
+                  <ArrowRight className={classname} />
+           </div>
+    </NavLink>
+
   );
 };
---
+++ src/pages/Community-page/Community.tsx
@@ -0,0 +1,11 @@
+
+
+const Community = () => {
+  return (
+    <div className="flex justify-center items-center text-bold text-[100px]">
+      Community
+    </div>
+  )
+}
+
+export default Community
