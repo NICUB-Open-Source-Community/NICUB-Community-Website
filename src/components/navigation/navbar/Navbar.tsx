28122290b64a64118ffd8cbd5e4b7679cf921686
AbdulHameedi <sanilinity507@gmail.com>
2025-01-29 13:51:31 +0100

    updat:correction

--- src/components/navigation/navbar/Navbar.tsx
+++ src/components/navigation/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import { NavLink } from "react-router-dom";
 
 interface ArrowRightProps {
   classname?: string;
-  text:string;
+  text: string;
 }
 const Navbar = () => {
   const { resources, community } = navItems[0];
@@ -19,38 +19,40 @@ const Navbar = () => {
           <img src="/logo.jpeg" alt="logo" className="w-[100px] h-[80px]" />
         </Link>
         <div className="flex items-center gap-1">
-       <NavLink to="/about"><p className={hoverStyles}>About Us</p></NavLink>
-       <div className="relative group">
-          <NavLink to="/resources"><p className={hoverStyles}>Resources</p></NavLink>
-          <SubNav items={resources} />
+          <NavLink to="/about">
+            <p className={hoverStyles}>About Us</p>
+          </NavLink>
+          <div className="relative group">
+            <NavLink to="/resources">
+              <p className={hoverStyles}>Resources</p>
+            </NavLink>
+            <SubNav items={resources} />
+          </div>
+          <NavLink to="/projects">
+            <p className={hoverStyles}>Projects</p>
+          </NavLink>
+          <div className="relative group">
+            <NavLink to="/community">
+              <p className={hoverStyles}>Community</p>
+            </NavLink>
+            <SubNav items={community} />
+          </div>
+          <NavLink to="/events">
+            <p className={hoverStyles}>Events</p>
+          </NavLink>
+          <JoinUs classname="hidden" text="Contact Us" />
         </div>
-        <NavLink to="/projects"><p className={hoverStyles}>Projects</p></NavLink>
-        <div className="relative group">
-          <NavLink to="/community"><p className={hoverStyles}>Community</p></NavLink>
-          <SubNav items={community} />
-        </div>
-        <NavLink to="/events"><p className={hoverStyles}>Events</p></NavLink>
-        <JoinUs classname="hidden" text="Contact Us" />
-      </div>
-<!--     <nav className="flex justify-between items-center gap-0 px-[50px] mx-auto py-4 shadow-lg ">
-      <Link to="/"> 
-        <img src="/logo.jpeg" alt="logo" className="w-[100px] h-[70px]" />
-      </Link>
-      </div>
-    </nav> -->
+      </nav>
+    </section>
   );
 };
-
 
 export default Navbar;
 export const JoinUs = ({ classname, text }: ArrowRightProps) => {
   return (
-//     <NavLink to="/contact-us">
-          <div className="w-fit bg-[#D9432C] text-white rounded-2xl py-1 px-4 cursor-pointer hover:bg-[#EE624D] transition duration-300 flex items-center gap-2">
-                  <p>{text}</p>
-                  <ArrowRight className={classname} />
-           </div>
-//     </NavLink>
-
+    <div className="w-fit bg-[#D9432C] text-white rounded-2xl py-1 px-4 cursor-pointer hover:bg-[#EE624D] transition duration-300 flex items-center gap-2">
+      <p>{text}</p>
+      <ArrowRight className={classname} />
+    </div>
   );
 };
