1fd005e2d76fda684ab9554c541aa6681921d9e0
Adepoju Wasiu <136388170+AdepojuWasiu@users.noreply.github.com>
2025-02-01 04:26:34 +0100

    Community page2 (#26)

---
+++ src/pages/Community-page/data/guideline.ts
@@ -0,0 +1,55 @@
+export const guidelines = [
+    {
+      title: "Respect and Inclusivity",
+      content: [
+        "Treat all members with kindness and respect, regardless of their background, skill level, or perspective.",
+        "Embrace diversity and create a welcoming atmosphere for everyone.",
+        "Avoid discriminatory language, hate speech, or any form of harassment.",
+      ],
+    },
+    {
+      title: "Collaboration and Support",
+      content: [
+        "Encourage knowledge-sharing and help others grow by offering constructive feedback and guidance.",
+        "Collaborate openly and respect differing opinions, focusing on solutions rather than conflicts.",
+        "Celebrate collective achievements and acknowledge individual contributions.",
+      ],
+    },
+    {
+      title: "Commitment to Learning",
+      content: [
+        "Stay open to new ideas, tools, and approaches.",
+        "Actively participate in training sessions, projects, and discussions to enhance your skills.",
+        "Be patient with yourself and others during the learning process.",
+      ],
+    },
+    {
+      title: "Integrity and Responsibility",
+      content: [
+        "Contribute original work and give credit where it's due when building on others' ideas.",
+        "Follow ethical practices in all your contributions, especially in open-source projects.",
+        "Honor deadlines and commitments to the best of your ability.",
+      ],
+    },
+    {
+      title: "Professionalism in Communication",
+      content: [
+        "Use clear, respectful, and professional language in all forms of communication.",
+        "Avoid spamming, self-promotion, or irrelevant discussions in community spaces.",
+      ],
+    },
+    {
+      title: "Giving Back",
+      content: [
+        "Experienced members are encouraged to mentor and guide others, helping to nurture the next generation of developers.",
+        "Contribute to open-source projects, share resources, and actively support the NICUB mission.",
+      ],
+    },
+    {
+      title: "Reporting Issues",
+      content: [
+        "If you witness or experience any behavior that violates these guidelines, report it to the community moderators for appropriate action.",
+        "All reports will be handled confidentially and with care.",
+      ],
+    },
+  ];
